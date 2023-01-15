df2628988246fe336c705c005fa6e11733f3073c
Shahriar Jamil <ms.jamil369@gmail.com>
2023-01-15 22:51:42 +0600

    first commit

--- 1. Bisection Method for finding root.c
+++ 1. Bisection Method for finding root.c
@@ -1,15 +1,22 @@
-// Total line of this code is (75)
+// Total line of this code is (80)
 #include<stdio.h>
 #include<math.h>
-int a,b,fixed_a,fixed_b,i=0,j,count,count1;  //Globally declaration
-float x=0,result,positive,negative,f_x,arr[50],less_or_greater_then_zero,zero;
+int fixed_a,fixed_b,i=0,j,count,count1;  //Globally declaration
+float a,b,x=0,f_x,arr[50],less_or_greater_then_zero,zero,f_a,f_b;
 
 /*(user-define-function) Calculating the equation*/
 //start
 float calculating_equation()
 {
-    result=(x*x*x)-x-11;
-    return result;
+    f_x=(x*x*x)-x-11;
+}
+//end
+
+/*(user-define-function) Calculating the formula of root*/
+//start
+float formula()
+{
+    x=(a+b)/2;
 }
 //end
 
@@ -34,28 +41,17 @@ int two_values()
 }
 //end
 
-/*(user-define-function) Checking a and b are positive or negative*/
-//start
-void positive_negative_values()
-{
-    if(fixed_a>0)   positive=a;
-    else    positive=b;
-    if(fixed_a<0)   negative=a;
-    else    negative=b;
-}
-//end
-
 /*(user-define-function) finding the root of the given equation*/
 //start
 int calculation_for_root()
 {
     while(1)
     {
-        x=(positive+negative)/2;
-        f_x=calculating_equation();
+        x=formula();    //value of root
+        f_x=calculating_equation(); //value of f(x)
         less_or_greater_then_zero=f_x;
         count1=0;
-        if(less_or_greater_then_zero<0)   //if f_x is negative, then convert it to positive to check twice in next for loop
+        if(f_x<0)   //if f_x is negative, then convert it to positive to check twice in next for loop
         {
             f_x=f_x*(-1);
             less_or_greater_then_zero=f_x;
@@ -71,10 +67,18 @@ int calculation_for_root()
             break;
         }
         if(count1!=0)    f_x=f_x*(-1);
-        printf("  %d          %.3f      %.3f       %.3f        %.3f\n",i,negative,positive,x,f_x);
+        printf("  %d          %.3f      %.3f       %.3f        %.3f\n",i,a,b,x,f_x);
         if(f_x==0  || less_or_greater_then_zero<=zero || count==1) return 0;
-        else if(f_x>0)  positive=x;
-        else if(f_x<0)  negative=x;
+        else if(f_x>0)
+        {
+            if(fixed_a>0) a=x;
+            else    b=x;
+        }
+        else
+        {
+            if(fixed_a<0) a=x;
+            else    b=x;
+        }
     }
 }
 //end
@@ -84,8 +88,8 @@ int calculation_for_root()
 int main()
 {
     two_values();   //To find two values with opposite sign
-
-    positive_negative_values(); //checking a and b are positive or negative
+    printf("As f(%d) and f(%d) are opposite sign\n",(int)a,(int)b);
+    printf("Therefore, the root lies between (%d,%d)\n\n",(int)a,(int)b);
 
     zero=0.001;
     printf("Assume approximately zero ~ %.3f\n\n",zero);
@@ -93,6 +97,7 @@ int main()
     printf("Interval       a          b           x           f(x)\n");
     printf("--------------------------------------------------------\n");
     calculation_for_root(); //finding the root of the given equation
+
     printf("The root is : %.3f\n",x);
     return 0;
 }
